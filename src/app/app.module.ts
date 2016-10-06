9ad589edd598bf97cff9486f52d7d658e183849a
austinlangdon <alangdon93@gmail.com>
2016-10-05 18:23:57 -0700

    imports Http, Material, and routing config. declares initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+
+import 'hammerjs';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    [MaterialModule.forRoot()],
+    routing
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    RestaurantsComponent,
+    RestaurantDetailComponent
+  ],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
