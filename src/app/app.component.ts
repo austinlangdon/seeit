c814e8d90fc9c7d23098e8822ea9010b9ac3b8aa
austinlangdon <alangdon93@gmail.com>
2016-10-24 22:29:51 -0700

    Modifies header and app components; brings header layout back into root app component template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { UserService } from './shared';
+import { User } from './shared';
 import { ApiService, RestaurantService } from './shared';
 
 import {Observable} from 'rxjs/Rx';
@@ -16,6 +17,30 @@ import 'rxjs/add/operator/map';
 export class AppComponent implements OnInit {
   
   isAuthenticated: boolean;
+  appTitle = 'Dishup';
+  currentUser: User;
+    
+
+    views: Object[] = [
+        {
+        name: 'Home',
+        description: '',
+        icon: 'home',
+        route: '/home'
+        },
+        {
+        name: 'Popular Dishes',
+        description: '',
+        icon: 'restaurant_menu',
+        route: '/trending'
+        },
+        {
+        name: 'My Favorites',
+        description: '',
+        icon: 'favorite', 
+        route: '/favorites'
+        }
+    ];
 
   constructor(
     private userService: UserService,
@@ -28,5 +53,15 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.userService.populate();
     this.isAuthenticated  = this.route.data['isAuthenticated'];
+
+    this.userService.currentUser.subscribe(user => {
+            this.currentUser = user;
+            console.log(user);  
+        });
   }
+
+  signOut() {
+        this.userService.purgeAuth();
+        this.router.navigateByUrl('/home');
+    }
 }
